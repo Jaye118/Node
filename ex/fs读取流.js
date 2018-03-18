12a03c75af29c82e924b49779ad2ba85d96cfd55
Jaye118 <18330697972@163.com>
2018-03-18 20:16:59 +0800

    pipe

--- ex/fs读取流.js
+++ ex/fs读取流.js
@@ -2,6 +2,9 @@ var fs = require('fs');
 
 var rs = fs.createReadStream('./data.txt');
 var ws = fs.createWriteStream('./data副本呀.txt');
-rs.on('data',function(chunk){
+/*rs.on('data',function(chunk){
     ws.write(chunk)
-});
+});*/
+
+// pipe方法 : 当读出数据全部写入后，才进行下一次读取。内存利用充分，但是读取时间较长
+rs.pipe(ws);
